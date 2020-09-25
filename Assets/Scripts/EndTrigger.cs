796ed9c6fe676a4312b56e348b935c3de8e99385
Animatron <beniseeger@web.de>
2020-09-25 12:10:40 +0200

    Commiting the first files of my testgame
    This is the commit for the test game

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameController gameController;
+
+    private void OnTriggerEnter()
+    {
+        Invoke("triggerLevelEnding", 0.75f);
+    }
+
+    private void triggerLevelEnding()
+    {
+        gameController.completeLevel();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private bool gameActive = true;
+    public GameObject completeLevelUI;
+
+    public void EndGame()
+    {
+        if (gameActive)
+        {
+            gameActive = false;
+            //Restart Game
+            Restart();
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void completeLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+}
